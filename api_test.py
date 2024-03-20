a794b1cb0603ee52627598ef4dd1480446f70ea8
DotCanDie <justin.lee9462@gmail.com>
2024-03-20 19:12:57 -0400

    fixed test puuids

--- api_test.py
+++ api_test.py
@@ -4,7 +4,10 @@ import os
 # api_key = "insert api via developer.riotgames.com"
 
 api_key = os.environ["RIOT_APP_API_KEY"]
-test_ids = "1dq1hI89Zgd__zcs8qkr3YaKdK35R4wj20YNB8ELdJL5_55XGPAch6g0KEiAAwFpfkeMjEnQ5HrWOg"
+test_id = "1dq1hI89Zgd__zcs8qkr3YaKdK35R4wj20YNB8ELdJL5_55XGPAch6g0KEiAAwFpfkeMjEnQ5HrWOg"
+#ren + jisung id
+test_ids = ["1dq1hI89Zgd__zcs8qkr3YaKdK35R4wj20YNB8ELdJL5_55XGPAch6g0KEiAAwFpfkeMjEnQ5HrWOg",
+            "_Mgiig0pdFVXxA2btc4XjF_hVSOW16JzLhZiBZRi6LJdxt-QAZJD5fIY7sGcmKfIzJp37HjQggTR7A"]
 accountv1url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"
 tftaccounturl = "https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/"
 
@@ -54,10 +57,11 @@ def username_finder(puuid):
 
 def list_username_finder(puuid_list):
     username_list = []
-    for puuid in puuid_list:
-        username_list.append(username_finder(puuid))
 
+    for puuid in puuid_list:
+        print(username_finder(puuid))
+        
     return username_list
 
-
-print(username_finder(test_ids))
+print(username_finder(test_id))
+list_username_finder(test_ids)
