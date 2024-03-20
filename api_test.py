ce9822f46075248cf1eca5c917916248fd3dfa36
DotCanDie <justin.lee9462@gmail.com>
2024-03-20 18:44:34 -0400

    api_test fun

--- api_test.py
+++ api_test.py
@@ -1,21 +1,20 @@
 import requests
 import os
-# sets the api key as the environment variable in your computer
+# sets the api key as the environment variable in your computerjisung
 # api_key = "insert api via developer.riotgames.com"
+
 api_key = os.environ["RIOT_APP_API_KEY"]
-
-
+test_ids = "1dq1hI89Zgd__zcs8qkr3YaKdK35R4wj20YNB8ELdJL5_55XGPAch6g0KEiAAwFpfkeMjEnQ5HrWOg"
 accountv1url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"
 tftaccounturl = "https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/"
+
 
 riotid = input("What is your Riot ID\n")
 parts = riotid.split('#')
 username = parts[0]
 tagline = parts[1] 
 accountv1url += username + "/" + tagline + "?api_key=" + api_key
-
 puuidresponse = requests.get(accountv1url)
-print(accountv1url)
 puuid = puuidresponse.json()['puuid']
 tftaccounturl += puuid + "?api_key=" + api_key
 response = requests.get(tftaccounturl)
@@ -41,6 +40,26 @@ tftgamelisturl = 'https://americas.api.riotgames.com/tft/match/v1/matches/by-puu
 count = input("How many game info\n")
 tftgamelisturl += puuid + '/ids?start=0&' 'count=' +count + '&api_key=' + api_key
 gameresponse = requests.get(tftgamelisturl)
-gameList = gameresponse.json()
 print(gameresponse.json())
 games = ''
+
+def username_finder(puuid):
+
+    tftaccounturl2 = "https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/"
+    tftaccounturl2 += puuid+"?api_key="+ api_key
+    print(puuid)
+    print(tftaccounturl2)
+    response = requests.get(tftaccounturl2)
+    id = response.json()['name']
+
+    return id
+
+def list_username_finder(puuid_list):
+    username_list = []
+    for puuid in puuid_list:
+        username_list.append(username_finder(puuid))
+
+    return username_list
+
+
+print(username_finder(test_ids))
